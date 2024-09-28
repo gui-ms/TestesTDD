113c0f2bac4760ea6b82f0efa0bea454cc3a3923
Guilherme Martins <gui.martins.soares@gmail.com>
2024-09-27 21:02:05 -0300

    Added initial files

---
+++ CalculadoraTestes/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+    private CalculadoraImp _calculadora;
+
+    public CalculadoraTestes()
+    {
+        _calculadora = new CalculadoraImp();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
